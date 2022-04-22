292af60a47726ff159bda1541dfd18fdebc2b453
Kevin Backhouse <kevinbackhouse@github.com>
2022-04-22 22:27:06 +0100

    Work in progress

---
+++ projects/ruby/build.sh
@@ -0,0 +1,37 @@
+#!/bin/bash -eu
+# Copyright 2022 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+################################################################################
+
+./autogen.sh
+cflags="$CFLAGS" optflags="" ./configure
+make -j $(nproc)
+make install -j $(nproc)
+
+ruby_version=$(basename `find . -name 'ruby-*.pc'` .pc)
+RUBY_LIB_DIR=$(pkg-config --variable=libdir $ruby_version)
+RUBY_LIBRARIES=$(pkg-config --variable=LIBRUBYARG_STATIC $ruby_version)
+RUBY_INCLUDES=$(pkg-config --cflags $ruby_version)
+
+cd $SRC/fuzz
+clang++ abstract-fuzzer.cpp ruby-fuzzer.cpp -o $OUT/fuzz_ruby \
+    -std=c++20 \
+#    -g -O0 -fdeclspec -fno-omit-frame-pointer -fno-common \
+#    -fsanitize=address,fuzzer \
+    -Wall \
+    -L${RUBY_LIB_DIR} \
+    ${RUBY_INCLUDES} \
+    ${RUBY_LIBRARIES} |
+    ${LIB_FUZZING_ENGINE}
